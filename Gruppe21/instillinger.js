54f40afebee458f798167dd7ecaff4a1a17d38bc
sebg22 <58253763+sebg22@users.noreply.github.com>
2020-02-14 10:54:12 +0100

    Add files via upload

--- Gruppe21/instillinger.js
+++ Gruppe21/instillinger.js
@@ -1,6 +1,32 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+var slider2 = document.getElementById("myRange2");
+var output2 = document.getElementById("demo2");
+output2.innerHTML = slider2.value;
+
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+}
+var slider3 = document.getElementById("myRange3");
+var output3 = document.getElementById("demo3");
+output3.innerHTML = slider3.value;
+
+slider3.oninput = function() {
+  output3.innerHTML = this.value;
+}
+
 function changeColor() {
 	var element = document.querySelector('.colorChanger')
 	
-	element.classList.add('green')
+	if (element.className == 'colorChanger green'){
+    element.className = 'colorChanger red'
+} else {
+    element.className = 'colorChanger green'
+}
 }
 console.log(changeColor)
